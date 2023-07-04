dbe6814a532dc74a76b422ca07087ceaf33501d1
Pablo Moreno <ehtomer@gmail.com>
2023-07-04 19:30:02 +0200

    okk

--- pages/data.js
+++ pages/data.js
@@ -3,133 +3,140 @@ let allData = [
     title: 'Autre temps',
     updated: '2021',
     url: '/autre-temps',
-    description: 'nananan',
+    description: '...',
     status: ''
   },
   {
     title: 'BG',
     updated: '2023',
     url: '/bg',
-    description: 'nananan',
+    description: '...',
     status: ''
   },
   {
     title: 'Captcha',
     updated: '2020',
     url: '/',
-    description: 'nananan',
+    description: '...',
     status: ''
   },
   {
     title: 'carta-para-dani',
     updated: '2023',
     url: '/carta-para-dani',
-    description: 'nananan',
+    description: '...',
     status: 'secret'
   },
   {
     title: 'cartography',
     updated: 'currently',
     url: '/cartography',
-    description: 'nananan',
+    description: '...',
     status: ''
   },
   {
     title: 'glace-sans-tain',
     updated: '2021',
     url: '/glace-sans-tain',
-    description: 'nananan',
+    description: '...',
     status: ''
   },
   {
     title: 'julietta y tristan',
     updated: '2021',
     url: '/juliettaytristan',
-    description: 'nananan',
+    description: '...',
     status: 'secret'
   },
   {
     title: 'k&p',
     updated: '2022',
     url: '/k&p',
-    description: 'nananan',
+    description: '...',
     status: 'secret'
   },
   {
     title: 'lyrics',
     updated: '2023',
     url: '/lyrics',
-    description: 'nananan',
+    description: '...',
     status: ''
   },
   {
     title: 'mega-cakes',
     updated: 'futur',
     url: '/mega-cakes',
-    description: 'nananan',
+    description: '...',
     status: ''
   },
   {
     title: 'my-computer',
     updated: 'currently',
     url: '/my-computer',
-    description: 'nananan',
+    description: '...',
     status: ''
   },
   {
     title: 'naia-x-pablo',
     updated: '2022',
     url: '/naia-x-pablo',
-    description: 'nananan',
+    description: '...',
     status: 'secret'
   },
   {
     title: 'poeme-mer',
     updated: '2019',
     url: '/poeme-mer',
-    description: 'nananan',
+    description: '...',
     status: ''
   },
   {
     title: 'sunday-sites',
     updated: 'currently',
     url: '/sunday-sites',
-    description: 'nananan',
+    description: '...',
     status: ''
   },
   {
     title: 'sunsets',
     updated: '2021',
     url: '/sunsets',
-    description: 'nananan',
+    description: '...',
     status: ''
   },
   {
     title: 'tott',
     updated: '2020',
     url: '/tott',
-    description: 'nananan',
+    description: '...',
     status: ''
   },
   {
     title: 'twenty-six-hour-a-day',
     updated: '2023',
     url: '/twenty-six-hour-a-day',
-    description: 'nananan',
+    description: '...',
     status: ''
   },
   {
     title: 'voeux2021',
     updated: '2020',
     url: '/voeux2021',
-    description: 'nananan',
+    description: '...',
     status: ''
   },
   {
     title: 'web-archive',
     updated: '2019',
     url: '/web-archive',
-    description: 'nananan',
+    description: '...',
+    status: ''
+  },
+  {
+    title: 'music',
+    updated: '2023',
+    url: '/music',
+    description: 'my music for doing things',
     status: ''
   }
 ]
