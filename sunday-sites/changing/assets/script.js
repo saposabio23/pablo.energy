1ddf9dbfbc188cced8c0185aedd0b95fd5f7c92d
Pablo Moreno <ehtomer@gmail.com>
2023-07-06 18:59:35 +0200

    fef

--- sunday-sites/changing/assets/script.js
+++ sunday-sites/changing/assets/script.js
@@ -1,13 +1,41 @@
 console.log("hello");
 
 function init() {
-  document.title = '1';
-  setTimeout(function () {
-    document.title = '2';
+    document.title = '5';
     setTimeout(function () {
-      document.title = '3';
-    }, 3000);
-  }, 3000);
+      document.title = '4';
+      setTimeout(function () {
+        document.title = '3';
+        setTimeout(function () {
+          document.title = '2';
+          setTimeout(function () {
+            document.title = '1';
+            setTimeout(function () {
+              document.title = 'okay we can start!';
+              setTimeout(function () {
+                document.title = 'so i had this idea ';
+                setTimeout(function () {
+                  document.title = 'of a tab title who ';
+                  setTimeout(function () {
+                    document.title = 'become the whole and only content';
+                    setTimeout(function () {
+                      document.title = 'of the website.';
+                    }, 1700);
+                  }, 1700);
+                }, 1700);
+              }, 1700);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
 }
 
-init()
+document.addEventListener("visibilitychange", function() {
+  if (document.hidden){
+    init()
+  } else {
+      console.log("Browser tab is visible")
+  }
+});
+
