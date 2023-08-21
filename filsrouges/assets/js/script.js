3e21072dea9200d2b456b6506045ceeef43dddd7
Pablo Moreno <ehtomer@gmail.com>
2023-08-21 19:29:29 +0200

    plus

---
+++ filsrouges/assets/js/script.js
@@ -0,0 +1,15 @@
+var start = function () {
+    var video = document.getElementById('video'),
+        vendorUrl = window.URL || window.webkitURL;
+
+    if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true })
+            .then(function (stream) {
+                video.srcObject = stream;
+            }).catch(function (error) {
+                console.log("Something went wrong!");
+            });
+    }
+}
+
+start();
--- lune/assets/js/mobile-height.js
+++ lune/assets/js/mobile-height.js
@@ -3,7 +3,8 @@ IPHONE HEIGHT
 calc(var(--vh, 1vh) * 100);
 // -------------------------------------- */
 
-window.onload = function mobileWindow() {
+
+function mobileWindow() {
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
   console.log("VH on mobiles", vh);
@@ -11,4 +12,5 @@ window.onload = function mobileWindow() {
 
 window.addEventListener("resize", mobileWindow, false);
 window.addEventListener("orientationchange", mobileWindow, false);
+window.onload = mobileWindow();
 
