2dbd6b74f70e079640c32a9f602cc7050a688691
Pablo Moreno <ehtomer@gmail.com>
2024-02-05 20:41:46 +0100

    upp

--- soutenance/script.js
+++ soutenance/script.js
@@ -1,11 +1,14 @@
 const iframe = document.querySelector("iframe");
 
 var index = 1;
+console.log(index);
 
 document.addEventListener("keydown", function (event) {
   if (event.code === "ArrowRight") {
     index++;
-
+    if (index >= 5) {
+      index = 5;
+    }
     console.log(index);
     iframe.setAttribute("src", index + ".html");
   } else if (event.code === "ArrowLeft") {
