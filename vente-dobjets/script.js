aff3083b5b85395ed6437fe983ac1bc41af1dfe9
Pablo Moreno <ehtomer@gmail.com>
2024-06-03 21:25:09 +0200

    upup

--- vente-dobjets/script.js
+++ vente-dobjets/script.js
@@ -1,48 +1,65 @@
 // REAR CAMERA PHONE
 
 // if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+var onAR = false;
 
-function launchCamera() {
-  const constraints = {
-    video: {
-      facingMode: "environment",
-    },
-  };
+function launchAR() {
+  if (onAR == true) {
+    var video = document.getElementById("video");
+    video.remove();
+    onAR = false;
+    document.getElementById("ARbutton").innerHTML = "Voir en R.A";
+  } else {
+    let videoAR = document.createElement("video");
+    videoAR.id = "video";
+    videoAR.setAttribute("autoplay", "");
+    document.body.appendChild(videoAR);
 
-  var video = document.getElementById("video"),
-    vendorURL = window.URL || window.webkitURL;
+    const constraints = {
+      video: {
+        facingMode: "environment",
+      },
+    };
 
-  if (navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices
-      .getUserMedia(constraints)
-      .then(function (stream) {
-        video.style.zIndex = "2";
-        video.srcObject = stream;
-      })
-      .catch(function (error) {
-        console.log("Something went wrong");
-      });
+    var video = document.getElementById("video"),
+      vendorURL = window.URL || window.webkitURL;
+
+    if (navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function (stream) {
+          video.srcObject = stream;
+        })
+        .catch(function (error) {
+          console.log("Something went wrong");
+        });
+    }
+
+    onAR = true;
+    document.getElementById("ARbutton").innerHTML = "Quitter la R.A";
+    console.log(onAR);
   }
 }
 
 // CHANGE BACKGROUNDS
-const myArray = ["bg-sol.JPG", "bg-table.JPG", "bg-air.JPG"];
-
 let objects = [
   {
     title: "Trépied 180cm",
     image: "tripode.png",
     price: "10",
+    text: "mailto:ehtomer@gmail.com?subject=VENTE D'OBJETS&body=Bonjour Pablo! Le trépied que tu as mis en vente m'intéresse. AIl est toujours dispo?%0D%0A%0D%0AJe crois voir que tu peux mettre une appareil photo directement sur lui, sans avoir besoin de fixation, c'est cool!%0D%0A %0D%0ABonne journée!",
   },
   {
     title: "Machine à yahourt",
     image: "yogurtera.png",
     price: "10",
+    text: "mailto:ehtomer@gmail.com?subject=VENTE D'OBJETS&body=Salut Pablo,%0D%0A%0D%0AJ'ai vu ta machine à yahourts, et je voudrais bien l'acheter pour en faire chez moi.%0D%0AEn plus je sais qu'on peut ajouter des saveurs avant de les faire pour qu'ils soient déjà sucrés ou vanillés.%0D%0A %0D%0AJ'attends ta réponse!",
   },
   {
     title: "Chaise bureau",
     image: "silla.png",
     price: "35",
+    text: "mailto:ehtomer@gmail.com?subject=VENTE D'OBJETS&body=Salut Pablo,%0D%0A%0D%0AJ'ai vu que tu vendais de choses et j'aimerais acheter ta chaise de bureau, histoire d'être mieux pour bosser chez moi.%0D%0A %0D%0ATu me diras. Salut!",
   },
 ];
 
@@ -50,47 +67,123 @@ var currentObject = 0;
 
 function nextObject() {
   currentObject++;
+  if (currentObject < objects.length) {
+    loadNext();
+  } else {
+    currentObject = 0;
+    loadNext();
+  }
   console.log(currentObject);
-  loadObject();
 }
 
 function previousObject() {
   currentObject--;
+  if (currentObject < 0) {
+    currentObject = objects.length - 1;
+    loadPrevious();
+  } else {
+    loadPrevious();
+  }
   console.log(currentObject);
-  loadObject();
 }
 
-function loadObject() {
-  if (currentObject < objects.length) {
-    document.getElementById("objectImage").classList.add("imageGo");
-    document.getElementById("objectImage").classList.remove("imageMove");
+function loadNext() {
+  document.getElementById("objectImage").classList.add("imageGo");
+  document.getElementById("objectImage").classList.remove("imageMove");
 
+  setTimeout(function () {
+    document.getElementById("objectImage").classList.remove("imageGo");
+    document.getElementById("objectImage").classList.add("hidden");
+    updateObject();
     setTimeout(function () {
-      document.getElementById("objectImage").classList.remove("imageGo");
-      const currentItem = objects[currentObject];
-      document.getElementById("objectTitle").innerHTML = currentItem.title;
-      document.getElementById("objectPrice").innerHTML =
-        currentItem.price + "€";
-      document.getElementById("objectImage").src = "media/" + currentItem.image;
-      document.getElementById("index").innerHTML = currentObject + 1;
-      document.getElementById("objectImage").classList.add("hidden");
-
+      document.getElementById("objectImage").classList.remove("hidden");
       setTimeout(function () {
-        document.getElementById("objectImage").classList.remove("hidden");
-        setTimeout(function () {
-          document.getElementById("objectImage").classList.add("imageMove");
-        }, 100);
+        document.getElementById("objectImage").classList.add("imageMove");
       }, 100);
     }, 100);
+  }, 100);
+}
+
+function loadPrevious() {
+  document.getElementById("objectImage").classList.remove("imageMove");
+
+  setTimeout(function () {
+    document.getElementById("objectImage").classList.add("imageGo");
+    document.getElementById("objectImage").classList.add("hidden");
+    updateObject();
+
+    setTimeout(function () {
+      document.getElementById("objectImage").classList.remove("hidden");
+      setTimeout(function () {
+        document.getElementById("objectImage").classList.remove("imageGo");
+
+        document.getElementById("objectImage").classList.add("imageMove");
+      }, 100);
+    }, 100);
+  }, 100);
+}
+
+function updateObject() {
+  const currentItem = objects[currentObject];
+  document.getElementById("objectTitle").innerHTML = currentItem.title;
+  document.getElementById("objectPrice").innerHTML = currentItem.price + "€";
+  document.getElementById("objectImage").src = "media/" + currentItem.image;
+  document.getElementById("index").innerHTML = currentObject + 1;
+  document.getElementById("emailButton").setAttribute("href", currentItem.text);
+}
+window.addEventListener("load", (event) => {
+  document.getElementById("indexLenght").innerHTML = objects.length;
+  updateObject();
+  document.getElementById("objectImage").classList.add("imageMove");
+});
+
+// CHANGE  BACKGROUND
+
+// function changeBackground() {
+//   for (let i = 0, max = bgButtons.length; i < max; i++) {
+//     bgButtons[i].classList.remove("bgSelected");
+//   }
+
+//   number = e.getAttribute("data-bg");
+//   document.querySelector(".bg").style.backgroundImage =
+//     "url(media/" + number + ".JPG)";
+//   e.classList.add("bgSelected");
+// }
+
+const bgList = ["bg-sol.JPG", "bg-table.JPG", "bg-air.JPG"];
+
+// Variable to keep track of the current index
+let currentIndex = 0;
+
+function changeBackground() {
+  if (onAR == true) {
+    var video = document.getElementById("video");
+    video.remove();
+    onAR = false;
+    document.getElementById("ARbutton").innerHTML = "Voir en RA";
+  }
+  if (currentIndex < bgList.length) {
+    const currentItem = bgList[currentIndex];
+    document.querySelector(".bg").style.backgroundImage =
+      "url(media/" + currentItem + ")";
+    currentIndex++;
   } else {
-    currentObject = 0;
-    loadObject();
+    currentIndex = 0;
+    changeBackground();
   }
 }
 
-window.addEventListener("load", (event) => {
-  document.getElementById("indexLenght").innerHTML = objects.length;
-  loadObject();
+document.addEventListener("keydown", function (event) {
+  if (event.code === "Space") {
+    event.preventDefault();
+    changeBackground();
+  } else if (event.code === "ArrowRight") {
+    event.preventDefault();
+    nextObject();
+  } else if (event.code === "ArrowLeft") {
+    event.preventDefault();
+    previousObject();
+  }
 });
 
 // var MAX_IMAGE_WIDTH = 200; // repalce magic number with const
